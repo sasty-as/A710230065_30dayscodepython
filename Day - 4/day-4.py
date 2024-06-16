77f644942810b95a60b6107776a6579623a1ac7e
sasty-as <a710230065@student.ums.ac.id>
2024-06-16 14:17:44 +0700

    Create day-4.py
    Percabangan if-else adalah struktur kontrol yang digunakan untuk membuat keputusan dalam program berdasarkan kondisi tertentu.

---
+++ Day - 4/day-4.py
@@ -0,0 +1,11 @@
+# Percabangan if dan else
+
+nilai = int(input("Masukkan nilai : "))
+
+if nilai >= 70 and nilai <= 100:
+    print('Lulus')
+else:
+    print('Tidak Lulus')
+ 
+# Jika kondisi yang diberikan bernilai benar (True), maka blok kode tertentu akan dijalankan. 
+# Jika kondisi tersebut bernilai salah (False), maka blok kode lainnya (yang ada di bagian else) akan dijalankan.
