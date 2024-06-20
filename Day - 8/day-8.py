c823129930982a7ecaa83592511e36ad12785380
sasty-as <a710230065@student.ums.ac.id>
2024-06-20 14:57:59 +0700

    Create day-8.py

---
+++ Day - 8/day-8.py
@@ -0,0 +1,19 @@
+# struktur data pada python
+
+# List
+buah_list = ["apel", "pisang", "mangga", "jeruk", "apel"]
+
+# Tuple
+bulan_tuple = ("Januari", "Februari", "Maret", "April", "Mei")
+
+# Set
+huruf_set = {"a", "b", "c", "d", "e"}
+
+# Dictionary
+usia_dict = {"John": 25, "Jane": 30, "Doe": 35}
+
+# Menampilkan hasil implementasi
+print("List buah:", buah_list)
+print("Tuple bulan:", bulan_tuple)
+print("Set huruf:", huruf_set)
+print("Dictionary usia:", usia_dict)
