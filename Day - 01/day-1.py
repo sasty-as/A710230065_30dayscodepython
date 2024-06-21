c35d092e99bcbe638ae6c990c065e96b725e9f5b
sasty-as <a710230065@student.ums.ac.id>
2024-06-21 14:15:06 +0700

    Create day-1.py

---
+++ Day - 01/day-1.py
@@ -0,0 +1,13 @@
+print('Hello')
+
+#input, output dan variabel
+nama = input('Siapa nama kamu : ')
+print ('hallo ', nama)
+
+print("Apa mata kuliah favoritmu ?")
+mk_fav = input()
+print("Oh, ternyata kamu suka", mk_fav)
+
+nama_depan = "Sasty"
+nama_belakang = "Azizah Souva"
+print(nama_depan, nama_belakang)
